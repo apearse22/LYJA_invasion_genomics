a6746941b98c06e71938e2e32331b716c3bb290f
Jessie Pelosi <52464709+jessiepelosi@users.noreply.github.com>
2026-03-13 09:53:37 -0700

    add gbif records

--- scripts/coordinate_maps.R
+++ scripts/coordinate_maps.R
@@ -29,7 +29,10 @@ invaded <- states_map %>%
            region == "texas" |
            region == "mississippi" |
            region == "louisiana" |
-           region == "oklahoma") # no samples in oklahoma, just for aesthetic purposes
+           region == "oklahoma" |
+           region == "north carolina" |
+           region == "tennessee" |
+           region == "virginia") # no samples in oklahoma, just for aesthetic purposes
 
 invaded_basemap <- ggplot(invaded, aes(long, lat, group=group)) +
   geom_polygon(fill='gray97', color='black') +
@@ -64,7 +67,7 @@ native_basemap <- ggplot(native, aes(long, lat, group=group)) +
 
 ### getting coordinates from supplmental information table 
 
-coordinates <- read.csv("data/lyja_si_coordinates.csv")
+coordinates <- read.csv("LYJA_invasion_genomics/files/lyja_si_coordinates.csv")
 coordinates <- coordinates %>% 
   mutate(Approximate.Latitude = as.numeric(Approximate.Latitude)) %>% 
   mutate(Approximate.Longitude = as.numeric(Approximate.Longitude))
@@ -98,7 +101,81 @@ native_coordinates_mapped <- native_basemap +
 
 ### stacking plots 
 
-stacked.maps <- ggarrange(invaded_coordinates_mapped, native_coordinates_mapped, ncol = 2, common.legend = TRUE, legend = "right")
+stacked.maps <- ggarrange(invaded_coordinates_mapped, native_coordinates_mapped, ncol = 1, common.legend = TRUE, legend = "right")
 
 
+## Adding points from GBIF to look at the progression of the invasion over time
+
+library(rgbif)
+
+gbif_occs <- occ_data(scientificName = "Lygodium japonicum", limit = 10000, continent = "north_america", year = '1900, 1950')
+gbif_occs.df <- gbif_occs$data
+gbif_occs_earlyInv <- gbif_occs.df %>% 
+  filter(decimalLongitude < 0)
+
+gbif_occs <- occ_data(scientificName = "Lygodium japonicum", limit = 10000, continent = "north_america", year = '1950, 1975')
+gbif_occs.df <- gbif_occs$data
+gbif_occs_MidInv <- gbif_occs.df %>% 
+  filter(decimalLongitude < 0)
+
+gbif_occs <- occ_data(scientificName = "Lygodium japonicum", limit = 10000, continent = "north_america", year = '1975, 2000')
+gbif_occs.df <- gbif_occs$data
+gbif_occs_lateMidInv <- gbif_occs.df %>% 
+  filter(decimalLongitude < 0)
+
+gbif_occs <- occ_data(scientificName = "Lygodium japonicum", limit = 10000, continent = "north_america", year = '2000, 2025')
+gbif_occs.df <- gbif_occs$data
+gbif_occs_lateInv <- gbif_occs.df %>% 
+  filter(decimalLongitude < 0)
   
+# early invasion 
+early_invasion_map <- invaded_basemap +
+  geom_point(gbif_occs_earlyInv, mapping=aes(x=decimalLongitude, y=decimalLatitude), 
+             group="Abbreviation", size= 2, color = "gray32") +
+  xlim(-110, -75) + ylim(25, 38) + ggtitle("1900-1950")
+
+early_invasion_map
+
+# early mid-invasion 
+Mid_invasion_map <- invaded_basemap +
+  geom_point(gbif_occs_earlyInv, mapping=aes(x=decimalLongitude, y=decimalLatitude), 
+             group="Abbreviation", size= 2, color = "gray32") +
+  geom_point(gbif_occs_earlyMidInv, mapping=aes(x=decimalLongitude, y=decimalLatitude), 
+             group="Abbreviation", size= 2, color = "gray32") +
+  geom_point(gbif_occs_MidInv, mapping=aes(x=decimalLongitude, y=decimalLatitude), 
+             group="Abbreviation", size= 2, color = "gray32") +
+  xlim(-110, -75) + ylim(25, 38) + ggtitle("1950-1975")
+
+Mid_invasion_map
+
+# Late mid invasion 
+lateMid_invasion_map <- invaded_basemap +
+  geom_point(gbif_occs_earlyInv, mapping=aes(x=decimalLongitude, y=decimalLatitude), 
+             group="Abbreviation", size= 2, color = "gray32") +
+  geom_point(gbif_occs_earlyMidInv, mapping=aes(x=decimalLongitude, y=decimalLatitude), 
+             group="Abbreviation", size= 2, color = "gray32") +
+  geom_point(gbif_occs_MidInv, mapping=aes(x=decimalLongitude, y=decimalLatitude), 
+             group="Abbreviation", size= 2, color = "gray32") +
+  geom_point(gbif_occs_lateMidInv, mapping = aes(x =decimalLongitude, y = decimalLatitude),
+             group="Abbreviation", size= 2, color = "gray32") +
+  xlim(-110, -75) + ylim(25, 38) + ggtitle("1975-2000")
+
+lateMid_invasion_map
+
+# Late invasion 
+late_invasion_map <- invaded_basemap +
+  geom_point(gbif_occs_earlyInv, mapping=aes(x=decimalLongitude, y=decimalLatitude), 
+             group="Abbreviation", size= 2, color = "gray32") +
+  geom_point(gbif_occs_earlyMidInv, mapping=aes(x=decimalLongitude, y=decimalLatitude), 
+             group="Abbreviation", size= 2, color = "gray32") +
+  geom_point(gbif_occs_MidInv, mapping=aes(x=decimalLongitude, y=decimalLatitude), 
+             group="Abbreviation", size= 2, color = "gray32") +
+  geom_point(gbif_occs_lateMidInv, mapping = aes(x =decimalLongitude, y = decimalLatitude),
+             group="Abbreviation", size= 2, color = "gray32") +
+  geom_point(gbif_occs_lateInv, mapping = aes(x =decimalLongitude, y = decimalLatitude),
+             group="Abbreviation", size= 2, color = "gray32") +
+  xlim(-110, -75) + ylim(25, 38) + ggtitle("2000-2025")
+
+late_invasion_map
+
+grid.arrange(early_invasion_map, Mid_invasion_map, lateMid_invasion_map, late_invasion_map)
