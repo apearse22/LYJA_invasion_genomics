b89824a37bcde97574499985201437a4e399622e
Abby Pearse <apearse22@arizona.edu>
2026-04-23 10:34:49 -0700

    moved transcriptome analyses

--- scripts/genetic_distance.R
+++ scripts/genetic_distance.R
@@ -20,7 +20,6 @@ samples.native <- c("SRR29127787", "SRR29127784", "SRR29127767","SRR29127785","S
                     "SRR29127773","SRR29127781","SRR29127778","SRR29127765", "SRR29127793", "SRR29127772")
 
 popmap <- read.csv("files/popmap_updatedcoords.csv")
-transcriptome <- read.vcfR("files/txm.50missing.CTmarked.recalc.maf0.05.thinned.vcf")
 captus <- read.vcfR("files/captus.SNPs.0.5missing.CTmarked.recalc.maf0.5.thinned.vcf")
 
 
@@ -72,20 +71,6 @@ popmap.nat.years <- popmap.nat.mat %>%
 
 ############################################################ Creating genind objects #############################################
 
-### Transcriptome
-
-transcriptome.genind <- vcfR2genind(transcriptome, ploidy = 4)
-
-transcriptome.genind.inv <- transcriptome.genind[!indNames(transcriptome.genind) %in% samples.native]
-transcriptome.genind.nat <- transcriptome.genind[indNames(transcriptome.genind) %in% samples.native & indNames(transcriptome.genind) != "SRR29127772"]
-
-# SRR...72 only needs to be removed for the geographic isolation because it has no coordinates, it's fine to include for temporal isolation. making a new genind
-
-transcriptome.genind.nat.temp <- transcriptome.genind[indNames(transcriptome.genind) %in% samples.native]
-
-
-### CAPTUS
-
 captus.genind <- vcfR2genind(captus, ploidy = 4)
 
 captus.genind.inv <- captus.genind[!indNames(captus.genind) %in% samples.native]
@@ -95,15 +80,6 @@ captus.genind.nat <- captus.genind[indNames(captus.genind) %in% samples.native &
 captus.genind.nat.temp <- captus.genind[indNames(captus.genind) %in% samples.native]
 
 ################################################# Calculating genetic distance matrices ###############################
-
-### Transcriptome invaded samples
-
-transcriptome.gen.dist.inv <- prevosti.dist(transcriptome.genind.inv)
-
-### Transcriptome native samples
-
-transcriptome.gen.dist.nat <- prevosti.dist(transcriptome.genind.nat)
-transcriptome.gen.dist.nat.temp <- prevosti.dist(transcriptome.genind.nat.temp) # temporal distance, including SRR29127772
 
 ### CAPTUS invaded samples
 
@@ -126,16 +102,6 @@ geo.dist.nat <- dist(popmap.nat.coords, method = "euclidean") # Native samples
 
 ########### Conducting isolation by distance Mantel tests
 
-# Transcriptome invaded samples
-
-mantel.transcriptome.inv <- mantel(transcriptome.gen.dist.inv, geo.dist.inv, method = "pearson", permutations = 999)
-summary(mantel.transcriptome.inv)
-
-# Transcriptome native samples
-
-mantel.transcriptome.nat <- mantel(transcriptome.gen.dist.nat, geo.dist.nat, method = "pearson", permutations = 999)
-summary(mantel.transcriptome.nat)
-
 # CAPTUS invaded samples
 
 mantel.captus.inv <- mantel(captus.gen.dist.inv, geo.dist.inv, method = "pearson", permutations = 999)
@@ -151,40 +117,6 @@ summary(mantel.captus.nat)
 
 geo.dist.inv.vec <- as.vector(geo.dist.inv)
 geo.dist.nat.vec <- as.vector(geo.dist.nat)
-
-### Transcriptome invaded samples
-
-transcriptome.gen.dist.inv.vec <- as.vector(transcriptome.gen.dist.inv)
-
-transcriptome.inv.geo.plot.df <- data.frame(
-  GenDist = transcriptome.gen.dist.inv.vec, 
-  GeoDist = geo.dist.inv.vec)
-
-
-transcriptome.inv.geo.plot <- ggplot(transcriptome.inv.geo.plot.df, aes(GeoDist, GenDist)) +
-  geom_point(color = "royalblue") + 
-  geom_smooth(method = "lm", color = "red") +
-  xlab("Geographic Distance") +
-  ylab("Genetic Distance") +
-  ggtitle("Invaded Transcriptome Isolation by Distance") +
-  theme_bw()
-  
-
-### Transcriptome native samples
-
-transcriptome.gen.dist.nat.vec <- as.vector(transcriptome.gen.dist.nat)
-
-transcriptome.nat.geo.plot.df <- data.frame(
-  GenDist = transcriptome.gen.dist.nat.vec, 
-  GeoDist = geo.dist.nat.vec)
-
-transcriptome.nat.geo.plot <- ggplot(transcriptome.nat.geo.plot.df, aes(GeoDist, GenDist)) +
-  geom_point(color = "royalblue") + 
-  geom_smooth(method = "lm", color = "red") +
-  xlab("Geographic Distance") +
-  ylab("Genetic Distance") +
-  ggtitle("Native Transcriptome Isolation by Distance") +
-  theme_bw()
 
 
 ### CAPTUS invaded samples
@@ -239,16 +171,6 @@ temp.dist.nat.vec <- as.vector(temp.dist.nat)
 
 ########### Conducting Mantel tests
 
-# Transcriptome invaded samples
-
-mantel.transcriptome.inv.temp <- mantel(transcriptome.gen.dist.inv, temp.dist.inv, method = "pearson", permutations = 999)
-summary(mantel.transcriptome.inv.temp)
-
-# Transcriptome native samples
-
-mantel.transcriptome.nat.temp <- mantel(transcriptome.gen.dist.nat.temp, temp.dist.nat, method = "pearson", permutations = 999)
-summary(mantel.transcriptome.nat.temp)
-
 # CAPTUS invaded samples
 
 mantel.captus.inv.temp <- mantel(captus.gen.dist.inv, temp.dist.inv, method = "pearson", permutations = 999)
@@ -262,39 +184,6 @@ summary(mantel.captus.nat.temp)
 
 
 ########### Plotting temporal isolation by distance
-
-
-### Invaded transcriptome
-
-transcriptome.inv.temp.plot.df <- data.frame(
-  GenDist = transcriptome.gen.dist.inv.vec, 
-  TempDist = temp.dist.inv.vec)
-
-transcriptome.inv.temp.plot <- ggplot(transcriptome.inv.temp.plot.df, aes(TempDist, GenDist)) +
-  geom_point(color = "royalblue") + 
-  geom_smooth(method = "lm", color = "red") +
-  xlab("Temporal Distance") +
-  ylab("Genetic Distance") +
-  ggtitle("Invaded Transcriptome Isolation by Time") +
-  theme_bw()
-
-
-### Native transcriptome
-
-transcriptome.gen.dist.nat.temp.vec <- as.vector(transcriptome.gen.dist.nat.temp)
-
-transcriptome.nat.temp.plot.df <- data.frame(
-  GenDist = transcriptome.gen.dist.nat.temp.vec, 
-  TempDist = temp.dist.nat.vec)
-
-transcriptome.nat.temp.plot <- ggplot(transcriptome.nat.temp.plot.df, aes(TempDist, GenDist)) +
-  geom_point(color = "royalblue") + 
-  geom_smooth(method = "lm", color = "red") +
-  xlab("Temporal Distance") +
-  ylab("Genetic Distance") +
-  ggtitle("Native Transcriptome Isolation by Time") +
-  theme_bw()
-
 
 ### Invaded CAPTUS
 
@@ -351,43 +240,6 @@ cp1 <- c("SRR29127726", "SRR29127727", "SRR29127728", "SRR29127729", "SRR2912773
 cp2 <- c("SRR29127768")
 cp3 <- c("SRR29127745", "SRR29127746")
 
-### Transcriptome
-
-transcriptome.inv <- as.matrix(transcriptome.gen.dist.inv)
-transcriptome.inv[upper.tri(transcriptome.inv, diag = FALSE)] <- NA
-dimnames(transcriptome.inv) <- list(rownames(transcriptome.inv), colnames(transcriptome.inv))
-
-transcriptome.inv.df <- as.data.frame(as.table(transcriptome.inv))
-transcriptome.inv.df <- na.omit(transcriptome.inv.df)
-
-transcriptome.inv.df <- transcriptome.inv.df %>% 
-  mutate(plastid_haplotype_1 = case_when(
-    Var1 %in% cp1 ~ "1",
-    Var1 %in% cp2 ~ "2",
-    Var1 %in% cp3 ~ "3",
-    TRUE            ~ "Not Found" 
-  )) %>% 
-  mutate(plastid_haplotype_2 = case_when (
-    Var2 %in% cp1 ~ "1", 
-    Var2 %in% cp2 ~ "2",
-    Var2 %in% cp3 ~ "3",
-    TRUE            ~ "Not Found" 
-  )) %>% 
-  mutate(haplotype_div = case_when (
-    plastid_haplotype_1 == plastid_haplotype_2 ~ "same", 
-    plastid_haplotype_1 != plastid_haplotype_2 ~ "different"
-  )) %>% 
-  filter(Freq != 0) %>% 
-  group_by(haplotype_div)
-
-transcriptome.cphaps.plot <- ggplot(transcriptome.inv.df, aes(x = haplotype_div, y = Freq, fill = haplotype_div)) +
-  geom_boxplot(alpha = 0.5) +
-  xlab("Same/Different Plastid Haplotype") + 
-  ylab("Genetic Distance") + 
-  ggtitle("Transcriptome") +
-  theme_bw()
-
-wilcox.test(Freq ~ haplotype_div, data = transcriptome.inv.df)
 
 ### CAPTUS
 
@@ -426,3 +278,156 @@ captus.cphaps.plot <- ggplot(captus.inv.df, aes(x = haplotype_div, y = Freq, fil
   theme_bw()
 
 wilcox.test(Freq ~ haplotype_div, data = captus.inv.df)
+
+
+
+
+
+############################################################# TRANSCRIPTOME ANALYSES ##############################################
+
+
+
+transcriptome <- read.vcfR("files/txm.50missing.CTmarked.recalc.maf0.05.thinned.vcf")
+### Transcriptome
+
+transcriptome.genind <- vcfR2genind(transcriptome, ploidy = 4)
+
+transcriptome.genind.inv <- transcriptome.genind[!indNames(transcriptome.genind) %in% samples.native]
+transcriptome.genind.nat <- transcriptome.genind[indNames(transcriptome.genind) %in% samples.native & indNames(transcriptome.genind) != "SRR29127772"]
+
+# SRR...72 only needs to be removed for the geographic isolation because it has no coordinates, it's fine to include for temporal isolation. making a new genind
+
+transcriptome.genind.nat.temp <- transcriptome.genind[indNames(transcriptome.genind) %in% samples.native]
+
+### Transcriptome invaded samples
+
+transcriptome.gen.dist.inv <- prevosti.dist(transcriptome.genind.inv)
+
+### Transcriptome native samples
+
+transcriptome.gen.dist.nat <- prevosti.dist(transcriptome.genind.nat)
+transcriptome.gen.dist.nat.temp <- prevosti.dist(transcriptome.genind.nat.temp) # temporal distance, including SRR29127772
+
+# Transcriptome invaded samples
+
+mantel.transcriptome.inv <- mantel(transcriptome.gen.dist.inv, geo.dist.inv, method = "pearson", permutations = 999)
+summary(mantel.transcriptome.inv)
+
+# Transcriptome native samples
+
+mantel.transcriptome.nat <- mantel(transcriptome.gen.dist.nat, geo.dist.nat, method = "pearson", permutations = 999)
+summary(mantel.transcriptome.nat)
+
+### Transcriptome invaded samples
+
+transcriptome.gen.dist.inv.vec <- as.vector(transcriptome.gen.dist.inv)
+
+transcriptome.inv.geo.plot.df <- data.frame(
+  GenDist = transcriptome.gen.dist.inv.vec, 
+  GeoDist = geo.dist.inv.vec)
+
+
+transcriptome.inv.geo.plot <- ggplot(transcriptome.inv.geo.plot.df, aes(GeoDist, GenDist)) +
+  geom_point(color = "royalblue") + 
+  geom_smooth(method = "lm", color = "red") +
+  xlab("Geographic Distance") +
+  ylab("Genetic Distance") +
+  ggtitle("Invaded Transcriptome Isolation by Distance") +
+  theme_bw()
+
+
+### Transcriptome native samples
+
+transcriptome.gen.dist.nat.vec <- as.vector(transcriptome.gen.dist.nat)
+
+transcriptome.nat.geo.plot.df <- data.frame(
+  GenDist = transcriptome.gen.dist.nat.vec, 
+  GeoDist = geo.dist.nat.vec)
+
+transcriptome.nat.geo.plot <- ggplot(transcriptome.nat.geo.plot.df, aes(GeoDist, GenDist)) +
+  geom_point(color = "royalblue") + 
+  geom_smooth(method = "lm", color = "red") +
+  xlab("Geographic Distance") +
+  ylab("Genetic Distance") +
+  ggtitle("Native Transcriptome Isolation by Distance") +
+  theme_bw()
+
+# Transcriptome invaded samples
+
+mantel.transcriptome.inv.temp <- mantel(transcriptome.gen.dist.inv, temp.dist.inv, method = "pearson", permutations = 999)
+summary(mantel.transcriptome.inv.temp)
+
+# Transcriptome native samples
+
+mantel.transcriptome.nat.temp <- mantel(transcriptome.gen.dist.nat.temp, temp.dist.nat, method = "pearson", permutations = 999)
+summary(mantel.transcriptome.nat.temp)
+
+
+### Invaded transcriptome
+
+transcriptome.inv.temp.plot.df <- data.frame(
+  GenDist = transcriptome.gen.dist.inv.vec, 
+  TempDist = temp.dist.inv.vec)
+
+transcriptome.inv.temp.plot <- ggplot(transcriptome.inv.temp.plot.df, aes(TempDist, GenDist)) +
+  geom_point(color = "royalblue") + 
+  geom_smooth(method = "lm", color = "red") +
+  xlab("Temporal Distance") +
+  ylab("Genetic Distance") +
+  ggtitle("Invaded Transcriptome Isolation by Time") +
+  theme_bw()
+
+
+### Native transcriptome
+
+transcriptome.gen.dist.nat.temp.vec <- as.vector(transcriptome.gen.dist.nat.temp)
+
+transcriptome.nat.temp.plot.df <- data.frame(
+  GenDist = transcriptome.gen.dist.nat.temp.vec, 
+  TempDist = temp.dist.nat.vec)
+
+transcriptome.nat.temp.plot <- ggplot(transcriptome.nat.temp.plot.df, aes(TempDist, GenDist)) +
+  geom_point(color = "royalblue") + 
+  geom_smooth(method = "lm", color = "red") +
+  xlab("Temporal Distance") +
+  ylab("Genetic Distance") +
+  ggtitle("Native Transcriptome Isolation by Time") +
+  theme_bw()
+
+### Transcriptome
+
+transcriptome.inv <- as.matrix(transcriptome.gen.dist.inv)
+transcriptome.inv[upper.tri(transcriptome.inv, diag = FALSE)] <- NA
+dimnames(transcriptome.inv) <- list(rownames(transcriptome.inv), colnames(transcriptome.inv))
+
+transcriptome.inv.df <- as.data.frame(as.table(transcriptome.inv))
+transcriptome.inv.df <- na.omit(transcriptome.inv.df)
+
+transcriptome.inv.df <- transcriptome.inv.df %>% 
+  mutate(plastid_haplotype_1 = case_when(
+    Var1 %in% cp1 ~ "1",
+    Var1 %in% cp2 ~ "2",
+    Var1 %in% cp3 ~ "3",
+    TRUE            ~ "Not Found" 
+  )) %>% 
+  mutate(plastid_haplotype_2 = case_when (
+    Var2 %in% cp1 ~ "1", 
+    Var2 %in% cp2 ~ "2",
+    Var2 %in% cp3 ~ "3",
+    TRUE            ~ "Not Found" 
+  )) %>% 
+  mutate(haplotype_div = case_when (
+    plastid_haplotype_1 == plastid_haplotype_2 ~ "same", 
+    plastid_haplotype_1 != plastid_haplotype_2 ~ "different"
+  )) %>% 
+  filter(Freq != 0) %>% 
+  group_by(haplotype_div)
+
+transcriptome.cphaps.plot <- ggplot(transcriptome.inv.df, aes(x = haplotype_div, y = Freq, fill = haplotype_div)) +
+  geom_boxplot(alpha = 0.5) +
+  xlab("Same/Different Plastid Haplotype") + 
+  ylab("Genetic Distance") + 
+  ggtitle("Transcriptome") +
+  theme_bw()
+
+wilcox.test(Freq ~ haplotype_div, data = transcriptome.inv.df)
