2cc4097c861608d76d78fa4c32f935391120b081
Abby Pearse <apearse22@arizona.edu>
2026-04-23 14:05:46 -0700

    moved transcriptome analyses

--- scripts/PCA_txm_captus.R
+++ scripts/PCA_txm_captus.R
@@ -20,17 +20,10 @@ library(poppr)
 
 ### Reading in files
 
-transcriptome.vcf <- read.vcfR("files/txm.50missing.CTmarked.recalc.maf0.05.thinned.vcf")
 captus.vcf <- read.vcfR("files/captus.SNPs.0.5missing.CTmarked.recalc.maf0.5.thinned.vcf")
 popmap <- read.csv("files/popmap_updatedcoords.csv")
 
 ####################################### Creating genind objects ##########################################
-
-### Transcriptome
-
-transcriptome.genind <- vcfR2genind(transcriptome.vcf, ploidy = 4)
-pop(transcriptome.genind) <- popmap$Temporal_Group
-transcriptome.genind.nomissing <- missingno(transcriptome.genind, type = "mean")
 
 ### CAPTUS
 
@@ -40,18 +33,6 @@ captus.genind.nomissing <- missingno(captus.genind, type = "mean")
 
 
 ############################################ Generating PCA dataframes ###########################################
-
-### Transcriptome
-
-pca.transcriptome <- dudi.pca(transcriptome.genind.nomissing, scale = FALSE, scannf = FALSE, nf = 50)
-
-pve.transcriptome <- (pca.transcriptome$eig / sum(pca.transcriptome$eig))*100
-pve.transcriptome <- round(pve.transcriptome, digits = 2)
-
-pca.transcriptome.df <- pca.transcriptome$li
-pca.transcriptome.df$Ind <- rownames(pca.transcriptome.df)
-pca.transcriptome.df.popmap <- inner_join(pca.transcriptome.df, popmap)
-
 
 ### CAPTUS
 
@@ -69,40 +50,6 @@ pca.captus.df.popmap <- inner_join(pca.captus.df, popmap)
 
 native.locs <- c("Japan", "China", "Indonesia", "Palau", "Philippines", "Taiwan", "Vietnam")
 invaded.locs <- c("Florida", "Alabama", "Arkansas", "Georgia", "Louisiana", "Mississippi", "South Carolina", "Texas")
-
-### Transcriptome
-
-# Collection year 
-
-pca.txm.df.inv <- filter(pca.transcriptome.df.popmap, Invaded == "Y")
-pca.txm.df.nat <- filter(pca.transcriptome.df.popmap, Invaded == "N")
-
-pca.transcriptome.collectionyear <- ggplot() +
-  geom_point(pca.txm.df.inv, mapping = aes(x = Axis1, y = Axis2, color = as.numeric(Collection.Year)), size = 3, alpha = 0.75) +
-  geom_point(pca.txm.df.nat, mapping = aes(x = Axis1, y = Axis2), color = "black", shape = 17, size = 2) +
-  scale_color_viridis_c(limits = range(pca.transcriptome.df.popmap$Collection.Year)) +
-  xlab(paste0("PC 1 (", pve.transcriptome[1], "% variation explained)")) +
-  ylab(paste0("PC 2 (", pve.transcriptome[2], "% variation explained)")) +
-  theme_bw() +
-  labs(color = "Collection Year") +
-  ggtitle("Transcriptome Reference")
-
-ggsave("transcriptome.collectionyear.pca.pdf", height = 6, width = 8)
-ggsave("transcriptome.collectionyear.pca.png", height = 6, width = 8, dpi = 300)
-
-# Location
-
-pca.transcriptome.location <- ggplot(pca.transcriptome.df.popmap, aes(Axis1, Axis2, color = Location, shape = Invaded)) +
-  geom_point(size = 2) +
-  scale_shape_manual(values = c(17, 16)) +
-  scale_color_paletteer_d("colorBlindness::paletteMartin") +
-  xlab(paste0("PC 1 (", pve.transcriptome[1], "% variation explained)")) +
-  ylab(paste0("PC 2 (", pve.transcriptome[2], "% variation explained)")) +
-  ggtitle("Transcriptome Reference") +
-  theme_bw()
-
-ggsave("transcriptome.location.pca.pdf", height = 6, width = 8)
-ggsave("transcriptome.location.pca.png", height = 6, width = 8, dpi = 300)
 
 ### CAPTUS
 
@@ -148,28 +95,12 @@ pacific.islands <- c("SRR29127777", "SRR29127778", "SRR29127780", "SRR29127781",
 
 ### Creating and subsetting new geninds
 
-# Transcriptome
-
-transcriptome.genind.noPI <- transcriptome.genind[!indNames(transcriptome.genind) %in% pacific.islands]
-transcriptome.genind.noPI.nomissing <- missingno(transcriptome.genind.noPI, type = "mean")
-
 # CAPTUS
 
 captus.genind.noPI <- captus.genind[!indNames(captus.genind) %in% pacific.islands]
 captus.genind.noPI.nomissing <- missingno(captus.genind.noPI, type = "mean")
 
 #################################### Creating PCA dataframes ###########################################
-
-### Transcriptome
-
-pca.transcriptome.noPI <- dudi.pca(transcriptome.genind.noPI.nomissing, scale = FALSE, scannf = FALSE, nf = 50)
-
-pve.transcriptome.noPI <- (pca.transcriptome.noPI$eig / sum(pca.transcriptome.noPI$eig))*100
-pve.transcriptome.noPI <- round(pve.transcriptome.noPI, digits = 2)
-
-pca.transcriptome.noPI.df <- pca.transcriptome.noPI$li
-pca.transcriptome.noPI.df$Ind <- rownames(pca.transcriptome.noPI.df)
-pca.transcriptome.noPI.df.popmap <- inner_join(pca.transcriptome.noPI.df, popmap)
 
 ### CAPTUS
 
@@ -184,35 +115,6 @@ pca.captus.noPI.df.popmap <- inner_join(pca.captus.noPI.df, popmap)
 
 
 ####################################### Plotting PCAs ################################
-
-### Transcriptome
-
-# Collection Year
-
-#pca.transcriptome.noPI.collectionYear <- ggplot(pca.transcriptome.noPI.df.popmap, aes(Axis1, Axis2, color = Collection.Year, shape = Invaded)) +
-#  geom_point(size = 3) +
-#  scale_color_viridis() +
-#  xlab(paste0("PC 1 (", pve.transcriptome.noPI[1], "% variation explained)")) +
-#  ylab(paste0("PC 2 (", pve.transcriptome.noPI[2], "% variation explained)")) +
-#  ggtitle("Transcriptome") +
-#  theme_bw()
-
-#ggsave("transcriptome.noPI.collectionyear.pca.pdf", width = 8, height = 6)
-
-# Location
-
-pca.transcriptome.noPI.location <- ggplot(pca.transcriptome.noPI.df.popmap, aes(Axis1, Axis2, color = Location, shape = Invaded)) +
-  geom_point(size = 3) +
-  scale_shape_manual(values = c(17,16)) +
-  scale_color_paletteer_d("colorBlindness::paletteMartin") +
-  xlab(paste0("PC 1 (", pve.transcriptome.noPI[1], "% variation explained)")) +
-  ylab(paste0("PC 2 (", pve.transcriptome.noPI[2], "% variation explained)")) +
-  ggtitle("Transcriptome Reference") +
-  theme_bw()
-
-ggsave("transcriptome.noPI.location.pca.pdf", width = 8, height = 6)
-ggsave("transcriptome.noPI.location.pca.png", width = 8, height = 6, dpi = 300)
-
 
 ### CAPTUS
 
@@ -271,3 +173,110 @@ pca.captus.tempgroups <- ggplot(pca.captus.df.popmap, aes(Axis1, Axis2, color = 
   ggtitle("CAPTUS PCA") 
 
 #ggsave("captus.temporalgroups.pdf", height = 6, width = 8)
+
+
+
+
+################################################# Transcriptome analyses ###############################################
+
+transcriptome.vcf <- read.vcfR("files/txm.50missing.CTmarked.recalc.maf0.05.thinned.vcf")
+
+
+### Transcriptome
+
+transcriptome.genind <- vcfR2genind(transcriptome.vcf, ploidy = 4)
+pop(transcriptome.genind) <- popmap$Temporal_Group
+transcriptome.genind.nomissing <- missingno(transcriptome.genind, type = "mean")
+
+### Transcriptome
+
+pca.transcriptome <- dudi.pca(transcriptome.genind.nomissing, scale = FALSE, scannf = FALSE, nf = 50)
+
+pve.transcriptome <- (pca.transcriptome$eig / sum(pca.transcriptome$eig))*100
+pve.transcriptome <- round(pve.transcriptome, digits = 2)
+
+pca.transcriptome.df <- pca.transcriptome$li
+pca.transcriptome.df$Ind <- rownames(pca.transcriptome.df)
+pca.transcriptome.df.popmap <- inner_join(pca.transcriptome.df, popmap)
+
+
+### Transcriptome
+
+# Collection year 
+
+pca.txm.df.inv <- filter(pca.transcriptome.df.popmap, Invaded == "Y")
+pca.txm.df.nat <- filter(pca.transcriptome.df.popmap, Invaded == "N")
+
+pca.transcriptome.collectionyear <- ggplot() +
+  geom_point(pca.txm.df.inv, mapping = aes(x = Axis1, y = Axis2, color = as.numeric(Collection.Year)), size = 3, alpha = 0.75) +
+  geom_point(pca.txm.df.nat, mapping = aes(x = Axis1, y = Axis2), color = "black", shape = 17, size = 2) +
+  scale_color_viridis_c(limits = range(pca.transcriptome.df.popmap$Collection.Year)) +
+  xlab(paste0("PC 1 (", pve.transcriptome[1], "% variation explained)")) +
+  ylab(paste0("PC 2 (", pve.transcriptome[2], "% variation explained)")) +
+  theme_bw() +
+  labs(color = "Collection Year") +
+  ggtitle("Transcriptome Reference")
+
+ggsave("transcriptome.collectionyear.pca.pdf", height = 6, width = 8)
+ggsave("transcriptome.collectionyear.pca.png", height = 6, width = 8, dpi = 300)
+
+# Location
+
+pca.transcriptome.location <- ggplot(pca.transcriptome.df.popmap, aes(Axis1, Axis2, color = Location, shape = Invaded)) +
+  geom_point(size = 2) +
+  scale_shape_manual(values = c(17, 16)) +
+  scale_color_paletteer_d("colorBlindness::paletteMartin") +
+  xlab(paste0("PC 1 (", pve.transcriptome[1], "% variation explained)")) +
+  ylab(paste0("PC 2 (", pve.transcriptome[2], "% variation explained)")) +
+  ggtitle("Transcriptome Reference") +
+  theme_bw()
+
+ggsave("transcriptome.location.pca.pdf", height = 6, width = 8)
+ggsave("transcriptome.location.pca.png", height = 6, width = 8, dpi = 300)
+
+
+
+# Transcriptome
+
+transcriptome.genind.noPI <- transcriptome.genind[!indNames(transcriptome.genind) %in% pacific.islands]
+transcriptome.genind.noPI.nomissing <- missingno(transcriptome.genind.noPI, type = "mean")
+
+### Transcriptome
+
+pca.transcriptome.noPI <- dudi.pca(transcriptome.genind.noPI.nomissing, scale = FALSE, scannf = FALSE, nf = 50)
+
+pve.transcriptome.noPI <- (pca.transcriptome.noPI$eig / sum(pca.transcriptome.noPI$eig))*100
+pve.transcriptome.noPI <- round(pve.transcriptome.noPI, digits = 2)
+
+pca.transcriptome.noPI.df <- pca.transcriptome.noPI$li
+pca.transcriptome.noPI.df$Ind <- rownames(pca.transcriptome.noPI.df)
+pca.transcriptome.noPI.df.popmap <- inner_join(pca.transcriptome.noPI.df, popmap)
+
+### Transcriptome
+
+# Collection Year
+
+#pca.transcriptome.noPI.collectionYear <- ggplot(pca.transcriptome.noPI.df.popmap, aes(Axis1, Axis2, color = Collection.Year, shape = Invaded)) +
+#  geom_point(size = 3) +
+#  scale_color_viridis() +
+#  xlab(paste0("PC 1 (", pve.transcriptome.noPI[1], "% variation explained)")) +
+#  ylab(paste0("PC 2 (", pve.transcriptome.noPI[2], "% variation explained)")) +
+#  ggtitle("Transcriptome") +
+#  theme_bw()
+
+#ggsave("transcriptome.noPI.collectionyear.pca.pdf", width = 8, height = 6)
+
+# Location
+
+pca.transcriptome.noPI.location <- ggplot(pca.transcriptome.noPI.df.popmap, aes(Axis1, Axis2, color = Location, shape = Invaded)) +
+  geom_point(size = 3) +
+  scale_shape_manual(values = c(17,16)) +
+  scale_color_paletteer_d("colorBlindness::paletteMartin") +
+  xlab(paste0("PC 1 (", pve.transcriptome.noPI[1], "% variation explained)")) +
+  ylab(paste0("PC 2 (", pve.transcriptome.noPI[2], "% variation explained)")) +
+  ggtitle("Transcriptome Reference") +
+  theme_bw()
+
+ggsave("transcriptome.noPI.location.pca.pdf", width = 8, height = 6)
+ggsave("transcriptome.noPI.location.pca.png", width = 8, height = 6, dpi = 300)
+
